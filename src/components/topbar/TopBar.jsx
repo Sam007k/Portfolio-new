74d71c41254a7bc5f05a850eea7badfa1557a64d
sam007k <58684781+Sam007k@users.noreply.github.com>
2023-07-19 12:21:20 +0000

    "Initial commit"

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,5 +1,28 @@
 import "./topBar.scss";
+import {Person, Mail} from '@material-ui/icons'
 
 export default function TopBar() {
-  return <div className="topbar"></div>;
+  return <div className="topbar" id='topbar'>
+    <div className='wrapper'>
+      <div className='left'>
+        <a href='#intro' className='logo'>genius.</a>
+        <div className='itemContainer'>
+          <Person className='icon'/>
+          <span>+44 924 92 22</span>
+        </div>
+        <div className='itemContainer'>
+          <Mail className='icon'/>
+          <span>safak@genius.com</span>
+        </div>
+      </div>
+      <div className='right'>
+        <div className='hamburger'>
+          <span className='line1'></span>
+          <span className='line2'></span>
+          <span className='line3'></span>
+        </div>
+      </div>
+
+    </div>
+  </div>;
 }
