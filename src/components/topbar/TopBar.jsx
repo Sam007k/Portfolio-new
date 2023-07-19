6fa81c980fb57e83b077a5ced0e1a17980e79b3f
sam007k <58684781+Sam007k@users.noreply.github.com>
2023-07-19 12:21:20 +0000

    basic_page_setup

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,5 +1,13 @@
 import "./topBar.scss";
 
 export default function TopBar() {
-  return <div className="topbar"></div>;
+  return <div className="topbar" id='topbar'>
+    <div className='wrapper'>
+      <div className='left'>
+        <a href='#intro'> asdasd</a>
+      </div>
+      <div className='right'></div>
+
+    </div>
+  </div>;
 }
