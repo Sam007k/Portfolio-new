46d13d0de96526bbe12b2a52480de27c81373f1f
Sameer <58684781+Sam007k@users.noreply.github.com>
2023-07-25 06:49:48 +0000

    Intro changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,32 @@
 import "./intro.scss";
+import { init } from 'ityped'
+import { useEffect, useRef } from "react";
+
 
 export default function Intro() {
-  return <div className="intro" id='intro'></div>;
+
+  const textRef= useRef();
+
+  useEffect(()=>{
+    init(textRef.current, { showCursor: true,backDelay: 1500,backSpeed: 60, strings: ["Developer","Designer"]});
+
+  },[])
+  return <div className="intro" id='intro'>
+    <div className="left">
+      <div className="imgContainer">
+        {/* <img src="https://github.com/safak/youtube/blob/react-portfolio/public/assets/man.png?raw=true" alt="" /> */}
+      </div>
+    </div>
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi There, I'm</h2>
+        <h1>S.</h1>
+        <h3>Freelance<span ref={textRef}></span></h3>
+      </div>
+      <a href='#portfolio' >
+      <img src='https://github.com/safak/youtube/blob/react-portfolio/public/assets/arrow.png?raw=true' alt='' />
+      </a>
+    </div>
+
+  </div>;
 }
